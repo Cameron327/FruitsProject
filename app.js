d9798201c090e7a5a37f3db39569939fab8189ef
Cameron <yeec327@gmail.com>
2021-05-12 16:07:37 -0700

    Successfully implemented the adding of multiple items to a db collection

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const fruitSchema = new mongoose.Schema({
 });
 
 // initialize the documents with the schema structure
+// Fruit is the name of the collection (Mongoose is smart because it will automatically make it plural)
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
@@ -21,8 +22,45 @@ const fruit = new Fruit ({
     rating: 8,
     review: "Great"
 });
-fruit.save();
+// fruit.save();
 
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 27
+});
+person.save();
+
+const kiwi = new Fruit ({
+    name: "Kiwi",
+    rating: 10,
+    review: "Excellent"
+});
+const orange = new Fruit ({
+    name: "orange",
+    rating: 8,
+    review: "Great"
+});
+const banana = new Fruit ({
+    name: "banana",
+    rating: 8,
+    review: "Great"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+    if (err){
+        console.log(err);
+    } else {
+        console.log("Successfully added all fruits to the fruitsDB");
+    }
+});
 
 
 // this function will find the documents from our node.js app
